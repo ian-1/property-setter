9a2fc274e41fb88e84af365bac5326ce19240a8b
ian-1 <ian.walton@portmoor.com>
2020-03-29 11:24:44 +0100

    add add_property & remove_property methods to Portfolio

--- lib/portfolio.rb
+++ lib/portfolio.rb
@@ -1,4 +1,8 @@
 # Stores properties
 class Portfolio
   def self.how_many_properties; end
+
+  def self.add_property; end
+
+  def self.remove_property; end
 end
--- spec/portfolio_spec.rb
+++ spec/portfolio_spec.rb
@@ -4,4 +4,12 @@ describe Portfolio do
   it '.how_many_properties' do
     expect(described_class).to respond_to(:how_many_properties)
   end
+
+  it '.add_property' do
+    expect(described_class).to respond_to(:add_property)
+  end
+
+  it '.remove_property' do
+    expect(described_class).to respond_to(:remove_property)
+  end
 end
